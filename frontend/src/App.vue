68ab9ab2101786dc51c83ad906ba634225346ce1
hnrqb <henriquebf8@gmail.com>
2024-09-17 16:02:46 -0300

    feat: template structure.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,49 @@
 <template>
-	<div id="app">
-		<h1>Versão Inicial</h1>
-	</div>
+  <div id="app">
+    <Header 
+		title="Cod3r - Base de Conhecimento" 
+		:hideToggle="false"
+		:hideUserDropdown="false"
+	/>
+    <Menu></Menu>
+    <Content></Content>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import Header from "@/components/template/Header.vue"
+import Menu from "@/components/template/Menu.vue"
+import Content from "@/components/template/Content.vue"
+import Footer from "@/components/template/Footer.vue"
+
 export default {
-	name: "App",
+  name: "App",
+  components: { Header, Menu, Content, Footer },
+  computed: mapState(['isMenuVisible'])
 }
 </script>
 
 <style>
+* {
+  font-family: "Lato", "sans-serif";
+}
 
+body {
+  margin: 0;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 60px 1fr 40px;
+  grid-template-columns: auto 1fr;
+  grid-template-areas: 
+    "header header"
+    "menu content"
+    "menu footer";
+}
 </style>
