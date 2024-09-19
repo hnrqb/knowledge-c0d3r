391045a6ef357ac2230aed49386740eaeb66186e
hnrqb <henriquebf8@gmail.com>
2024-09-19 15:32:49 -0300

    feat: implement user module

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -1,19 +1,30 @@
 <template>
     <div class="admin-pages">
-        <PageTitle 
-            icon="fa fa-home"
-            main="Administração do Sistema"
-            sub="Cadastros & Cia"
-        >
+        <PageTitle icon="fa fa-home" main="Administração do Sistema"
+            sub="Cadastros & Cia">
         </PageTitle>
+        <div class="admin-pages-tabs" ref="pageTop">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Categorias"> 
+                        <CategoryAdmin></CategoryAdmin>
+                    </b-tab>
+                    <b-tab title="Usuários"> 
+                        <UserAdmin></UserAdmin>
+                    </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 import PageTitle from '../template/PageTitle'
+import CategoryAdmin from './CategoryAdmin'
+import UserAdmin from './UserAdmin'
 
 export default {
     name: 'AdminPages',
-    components: { PageTitle }
+    components: { PageTitle, CategoryAdmin, UserAdmin }
 }
 </script>
